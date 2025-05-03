d4efe608412b374a25df55426de953fd2d4990ef
Charley3D <charley.makevideo@gmail.com>
2025-05-03 15:35:33 +0200

    feat(operators): Add RemoveLipSync and RemoveNodeGroups operations
    Introduced operations for removing Lip Sync properties and node groups from objects and materials in Blender.
    These changes include new classes and methods to clean up Lip Sync custom properties and associated nodes, ensuring better project manageability and flexibility.

--- Operators/LIPSYNC2D_OT_SetMouthArea.py
+++ Operators/LIPSYNC2D_OT_SetMouthArea.py
@@ -5,33 +5,36 @@ import bmesh
 import bpy
 import mathutils
 
-from .LIPSYNC2D_OT_SetCustomProperties import add_default_image_spritesheet, add_spritesheet_node_to_mat, create_spritesheet_nodes, get_or_create_material
+from .LIPSYNC2D_OT_SetCustomProperties import add_default_image_spritesheet, add_spritesheet_node_to_mat, \
+    create_spritesheet_nodes, get_or_create_material
 
 
 class ViewState(TypedDict):
     location: mathutils.Vector
     rotation: mathutils.Quaternion
     distance: float
-    perspective: Literal['PERSP','ORTHO','CAMERA']
+    perspective: Literal['PERSP', 'ORTHO', 'CAMERA']
+
 
 class LIPSYNC2D_OT_SetMouthArea(bpy.types.Operator):
-    bl_idname="mesh.set_lips_area"
-    bl_label ="Set Lips Area"
-    bl_options ={'REGISTER','UNDO'}
+    bl_idname = "mesh.set_lips_area"
+    bl_label = "Set Lips Area"
+    bl_options = {'REGISTER', 'UNDO'}
 
     @classmethod
     def poll(cls, context):
         obj = context.active_object
         return obj is not None and obj.mode == 'EDIT' and obj.type == 'MESH'
-    
-    def execute(self, context: bpy.types.Context) -> set[Literal['RUNNING_MODAL', 'CANCELLED', 'FINISHED', 'PASS_THROUGH', 'INTERFACE']]:
+
+    def execute(self, context: bpy.types.Context) -> set[
+        Literal['RUNNING_MODAL', 'CANCELLED', 'FINISHED', 'PASS_THROUGH', 'INTERFACE']]:
         obj = context.active_object
 
         if obj is None:
             return {'CANCELLED'}
 
         if obj and obj.mode == 'EDIT' and isinstance(obj.data, bpy.types.Mesh):
-            
+
             mesh = obj.data
             bm = bmesh.from_edit_mesh(mesh)
 
@@ -41,7 +44,8 @@ class LIPSYNC2D_OT_SetMouthArea(bpy.types.Operator):
 
             if view_state is not None and context.area is not None:
                 activate_area_info = get_area_identifier(context.area)
-                bpy.app.timers.register(functools.partial(uv_unwrap_selection, activate_area_info, view_state), first_interval=.01)
+                bpy.app.timers.register(functools.partial(uv_unwrap_selection, activate_area_info, view_state),
+                                        first_interval=.01)
 
         return {'FINISHED'}
 
@@ -64,14 +68,14 @@ class LIPSYNC2D_OT_SetMouthArea(bpy.types.Operator):
 
         view_state = align_view_to_selection(quat)
         return view_state
-    
+
     @staticmethod
     def edit_face_material(context: bpy.types.Context, obj: bpy.types.Object, bm: bmesh.types.BMesh):
         face_material_indices = [f.material_index for f in bm.faces if f.select]
         material_index = face_material_indices[0] if len(face_material_indices) > 0 else -1
 
         main_material = get_or_create_material(obj, material_index)
-        obj.lipsync2d_props.lip_sync_2d_main_material = main_material # type: ignore
+        obj.lipsync2d_props.lip_sync_2d_main_material = main_material  # type: ignore
 
         nodes_spritesheet_reader = get_spritesheet_reader_from_mat(main_material)
 
@@ -80,52 +84,52 @@ class LIPSYNC2D_OT_SetMouthArea(bpy.types.Operator):
 
             if nodes_spritesheet_reader is None:
                 return {'CANCELLED'}
-            
+
             add_spritesheet_node_to_mat(context.active_object, main_material, nodes_spritesheet_reader)
-            context.active_object.lipsync2d_props.lip_sync_2d_sprite_sheet = add_default_image_spritesheet() #type: ignore
-    
+            context.active_object.lipsync2d_props.lip_sync_2d_sprite_sheet = add_default_image_spritesheet()  # type: ignore
+
 
 def get_spritesheet_reader_from_mat(main_material: bpy.types.Material):
     material_node_tree = main_material.node_tree
     nodes_spritesheet_reader = None
 
     if material_node_tree is not None:
-        nodes_spritesheet_reader = material_node_tree.nodes.get("cgp_spritesheet_reader")
+        nodes_spritesheet_reader = material_node_tree.nodes.get("cgp_main_group")
 
-        
     return nodes_spritesheet_reader
 
-def uv_unwrap_selection(activate_area_info:tuple[float, float, float, float, str], view_state: ViewState) -> None:
+
+def uv_unwrap_selection(activate_area_info: tuple[float, float, float, float, str], view_state: ViewState) -> None:
     obj = bpy.context.active_object
     if obj is None or not isinstance(obj.data, bpy.types.Mesh):
         return
-    
+
     if bpy.context.window is None:
         return
-    
+
     mesh = obj.data
     bm = bmesh.from_edit_mesh(mesh)
     selected_faces = [f for f in bm.faces if f.select]
-    
+
     uv_layer = None
     uv_layer_name = 'Mouth'
     previous_active_uv = mesh.uv_layers.active_index
-    
+
     if not uv_layer_name:
         return
-    
+
     uv_layer = bm.loops.layers.uv.get(uv_layer_name)
     if uv_layer is None:
         uv_layer = bm.loops.layers.uv.new(uv_layer_name)
-    
+
     if not uv_layer:
         raise Exception("No UV map found.")
-        
+
     mesh = obj.data
     uv_index = mesh.uv_layers.find(uv_layer_name)
     if uv_index != -1:
         mesh.uv_layers.active_index = uv_index
-    
+
     for area in bpy.context.window.screen.areas:
         if get_area_identifier(area) == activate_area_info:
             for region in area.regions:
@@ -135,13 +139,13 @@ def uv_unwrap_selection(activate_area_info:tuple[float, float, float, float, str
                     override["region"] = region
                     if area.spaces.active is None:
                         break
-                    space = cast( bpy.types.SpaceView3D, area.spaces.active)
+                    space = cast(bpy.types.SpaceView3D, area.spaces.active)
                     override["space_data"] = space
                     override["region_data"] = space.region_3d
 
                     with bpy.context.temp_override(**override):
                         bpy.ops.uv.project_from_view(orthographic=True, scale_to_bounds=True)
-                    #TODO: Unwrap unselected faces and move them out
+                    # TODO: Unwrap unselected faces and move them out
                     unselected = [f for f in bm.faces if not f.select]
                     for e in bm.edges:
                         e.select_set(False)
@@ -154,27 +158,29 @@ def uv_unwrap_selection(activate_area_info:tuple[float, float, float, float, str
 
                     for f in unselected:
                         f.select_set(True)
-                    
+
                     bpy.ops.uv.unwrap()
-                    
+
                     for face in unselected:
                         for loop in face.loops:
-                            loop[uv_layer].uv = mathutils.Vector([-.1,0])
-                            #TODO: Rotate uv island to cancel view rotation
+                            loop[uv_layer].uv = mathutils.Vector([-.1, 0])
+                            # TODO: Rotate uv island to cancel view rotation
 
                     for f in unselected:
                         f.select_set(False)
 
                     for f in selected_faces:
                         f.select_set(True)
-                    
+
                     break
-        
+
     mesh.uv_layers.active_index = previous_active_uv
     return None
 
+
 def get_area_identifier(area: bpy.types.Area):
     return (area.x, area.y, area.width, area.height, area.type)
+
 
 def save_view_state(region_3d: bpy.types.RegionView3D) -> ViewState:
     return {
@@ -184,19 +190,20 @@ def save_view_state(region_3d: bpy.types.RegionView3D) -> ViewState:
         "perspective": region_3d.view_perspective,
     }
 
+
 def restore_view_state(region_3d: bpy.types.RegionView3D, view_state: ViewState, only_perspective: bool = True):
     if only_perspective is False:
         region_3d.view_location = view_state["location"]
         region_3d.view_rotation = view_state["rotation"]
         region_3d.view_distance = view_state["distance"]
-    
+
     region_3d.view_perspective = view_state["perspective"]
 
 
 def align_view_to_selection(quat: mathutils.Quaternion) -> ViewState | None:
     if bpy.context.window is None:
         return None
-    
+
     for area in bpy.context.window.screen.areas:
         if area.type == 'VIEW_3D':
             for region in area.regions:
@@ -207,5 +214,5 @@ def align_view_to_selection(quat: mathutils.Quaternion) -> ViewState | None:
 
                     view_state = save_view_state(region_3d)
                     region_3d.view_rotation = quat
-                    return  view_state
+                    return view_state
     return None
