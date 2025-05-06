662e805cfaea52f6e4a74d663a0041b4d8dadc5d
Charley3D <charley.makevideo@gmail.com>
2025-05-06 13:45:49 +0200

    Bump version during deployment stage

---
+++ scripts/update_version.py
@@ -0,0 +1,13 @@
+import sys
+from tomlkit import parse, dumps  # type: ignore
+
+version = sys.argv[1]
+toml_path = "../blender_manifest.toml"
+
+with open(toml_path, "r", encoding="utf-8") as f:
+    doc = parse(f.read())
+
+doc["version"] = version
+
+with open(toml_path, "w", encoding="utf-8") as f:
+    f.write(dumps(doc))
