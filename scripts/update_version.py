f6eb3140897715f1d3fe3e432f401196e52a5b18
Charley3D <charley.makevideo@gmail.com>
2025-05-06 13:48:47 +0200

    update blender manifest path

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -2,7 +2,7 @@ import sys
 from tomlkit import parse, dumps  # type: ignore
 
 version = sys.argv[1]
-toml_path = "../blender_manifest.toml"
+toml_path = "blender_manifest.toml"
 
 with open(toml_path, "r", encoding="utf-8") as f:
     doc = parse(f.read())
